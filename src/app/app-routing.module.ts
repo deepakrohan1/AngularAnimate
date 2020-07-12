0b6f596ae4034de85e6e5309dfb4aaf1dba0784c
Deepak Rohan Sekar <deepakrohan@Deepaks-iMac.local>
2020-07-12 15:43:56 -0400

    intial layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BasicComponent } from './basic/basic.component';
+import { ContainedComponent } from './contained/contained.component';
+import { AdvComponent } from './adv/adv.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'animation-home',
+    component: HomeComponent
+  },
+  {
+    path: 'animation-basics',
+    component: BasicComponent
+  },
+  {
+    path: 'animation-contained',
+    component: ContainedComponent
+  },
+  {
+    path: 'animation-advanced',
+    component: AdvComponent
+  },
+  {
+    path: '',
+    redirectTo: '/animation-home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AdvComponent } from './adv/adv.component';
+import { BasicComponent } from './basic/basic.component';
+import { ContainedComponent } from './contained/contained.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdvComponent,
+    BasicComponent,
+    ContainedComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
